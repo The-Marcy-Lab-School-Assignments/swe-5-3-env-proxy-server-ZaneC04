13acaf710f8713d256b48bc5fcc2e13c35a01ee2
ZaneC04 <zenithzsc@gmail.com>
2026-03-10 15:56:32 -0400

    finished serveGifs and serveSearchGifs

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const serveStatic = express.static(pathToFrontend);
 
 const serveGifs = async (req, res, next) => {
     try {
-        const response = await fetch(`https://api.giphy.com/v1/gifs/trending?limit=3&rating=g&api_key=${process.env.API_KEY}`)
+        const response = await fetch(`https://api.giphy.com/v1/gifs/trending?limit=10&rating=g&api_key=${process.env.API_KEY}`)
         if (!response.ok) {
             throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
         }
@@ -37,12 +37,28 @@ const serveGifs = async (req, res, next) => {
     }
 }
 
+const serveSearchGifs = async (req, res, next) => {
+    const searchTerm = req.query.q
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${process.env.API_KEY}`)
+        if (!response.ok) {
+            throw new Error(`Fetch Failed: ${response.status} ${response.statusText}`,);
+        }
+        const data = await response.json()
+        res.send(data)
+    }
+    catch (error) {
+        res.status(503).send(error)
+    }
+}
+
 
 
 
 app.use(serveStatic);
 
 app.get("/api/gifs", serveGifs)
+app.get("/api/gifs/search", serveSearchGifs)
 
 //////////////////////////
 // Listener
